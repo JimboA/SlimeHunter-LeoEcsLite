45fa9ee49b4b891b13a1d1b6a917ae9b01d27e08
JimboA <47698839+JimboA@users.noreply.github.com>
2022-08-29 20:50:40 +0700

    Board editor

---
+++ Assets/BoardEditor/Code/Editor/BoardBrush.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+
+namespace BoardEditor
+{
+    [CustomGridBrush(false, false, false, "BattleBoardBrush")]
+    public class BoardBrush : GridBrush
+    {
+        [SerializeField] private BoardCreator _creator;
+
+        private void OnEnable()
+        {
+            if (_creator == null)
+                _creator = FindObjectOfType<BoardCreator>();
+            
+            if(_creator == null)
+                Debug.LogError($"Can't find BoardCreator script on scene: {SceneManager.GetActiveScene().name}");
+        }
+        
+        public override void Paint(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
+        {
+            if (!_creator.CheckBoardBounds(position))
+                return;
+
+            base.Paint(gridLayout, brushTarget, position);
+        }
+    }
+    
+    [CustomEditor(typeof(BoardBrush))]
+    public class SlimeHunterBrushDrawer : GridBrushEditor
+    {
+        public void OnEnable()
+        {
+            base.OnEnable();
+            Selection.selectionChanged += Repaint;
+        }
+
+        public void OnDisable()
+        {
+            base.OnDisable();
+            Selection.selectionChanged -= Repaint;
+        }
+    }
+}
---
+++ Assets/BoardEditor/Code/Editor/BoardCreatorInspector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BoardEditor
+{
+    [CustomEditor(typeof(BoardCreator))]
+    public class BoardCreatorInspector : UnityEditor.Editor
+    {
+        private BoardCreator _creator;
+
+        private void OnEnable()
+        {
+            _creator = (BoardCreator)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            GUILayout.Space(5);
+            if (GUILayout.Button("Save Board"))
+            {
+                _creator.GenerateBoard();
+            }
+        }
+    }
+}
